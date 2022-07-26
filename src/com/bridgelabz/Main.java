8595909a4f928bfc051284d7e1ab431a0868292e
Surabhi951 <surabhibhagat95@gmail.com>
2022-07-26 09:18:29 +0530

    Added three Integer Maximum Number

--- src/com/bridgelabz/Main.java
+++ src/com/bridgelabz/Main.java
@@ -3,5 +3,7 @@ package com.bridgelabz;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to test maximum of three numbers");
+        Integer x =10, y = 20, z = 30;
+        FindMaxValue.testMaximum(x , y , z);
     }
 }
