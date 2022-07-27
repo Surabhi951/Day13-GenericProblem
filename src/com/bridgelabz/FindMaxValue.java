b05d060950fdf1b6f706fa70c64d7d24fcabbb27
Surabhi951 <surabhibhagat95@gmail.com>
2022-07-27 21:40:20 +0530

    Added print max method

--- src/com/bridgelabz/FindMaxValue.java
+++ src/com/bridgelabz/FindMaxValue.java
@@ -1,17 +1,7 @@
 package com.bridgelabz;
 
 public class FindMaxValue<E extends Comparable<E>>{
-    E a1 , a2, a3, a4, a5;
-    public FindMaxValue(E a1, E a2, E a3, E a4, E a5) {
-        this.a1 = a1;
-        this.a2 = a2;
-        this.a3 = a3;
-        this.a4 = a4;
-        this.a5 = a5;
-
-    }
-
-    public static <E extends Comparable<E>> E testMaximum(E a1, E a2, E a3,  E a4, E a5) {
+    void testMaximum(E a1, E a2, E a3,  E a4, E a5) {
         E max = a1;
         if (a2.compareTo(max) > 0)
             max = a2;
@@ -21,13 +11,19 @@ public class FindMaxValue<E extends Comparable<E>>{
             max = a4;
         if (a5.compareTo(max) > 0)
             max = a5;
-        return max;
+        printMax(max);
+    }
+
+     void printMax(E max){
+        System.out.println("maximum value is "+max);
     }
     public static void main(String[] args) {
+
         System.out.println("Welcome to test maximum of three numbers");
-        System.out.println("The maximum of integer is: " +testMaximum(30,20,15,25,35));
-        System.out.println("The maximum of float is: " +testMaximum(4.9f, 1.7f, 9.6f,1.8f,2.4f));
-        System.out.println("The maximum of string is: " +testMaximum("Apple", "Peach", "Banana","Mango","Pineapple"));
+        FindMaxValue findMaxValue1 = new FindMaxValue();
+        findMaxValue1.testMaximum(30,20,15,25,35);
+        findMaxValue1.testMaximum(4.9f, 1.7f, 9.6f,1.8f,2.4f);
+        findMaxValue1.testMaximum("Apple", "Peach", "Banana","Mango","Pineapple");
 
     }
 }
